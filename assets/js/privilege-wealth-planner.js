57ea13fe61ba3f80159d7596eff79f0a6a20f1e9
Mohammad Mahdi Mohammadi <phoneticem@gmail.com>
2023-02-18 16:39:32 +0330

    changes

--- assets/js/privilege-wealth-planner.js
+++ assets/js/privilege-wealth-planner.js
@@ -11,5 +11,28 @@ $(function () {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            }
+        }
     });
+    const collapse = document.getElementById('collapseExample2')
+    collapse.addEventListener('hidden.bs.collapse', function () {
+        $(".reportCollapse .collapsePlus").fadeIn(1);
+        $(".reportCollapse .collapseMinus").fadeOut(1);
+    })
+    collapse.addEventListener('show.bs.collapse', function () {
+        $(".reportCollapse .collapsePlus").fadeOut(1);
+        $(".reportCollapse .collapseMinus").fadeIn(1);
+    })
 })
